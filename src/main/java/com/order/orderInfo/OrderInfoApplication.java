018147f9a19b37fed98d8071dd8402ef51acdbc5
mitrasupriya <mitrasupriya3@gmail.com>
2020-12-04 01:18:15 +0530

    initial commit

---
+++ src/main/java/com/order/orderInfo/OrderInfoApplication.java
@@ -0,0 +1,13 @@
+package com.order.orderInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order/orderInfo/OrderInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.orderInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
